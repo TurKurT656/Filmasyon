0c51158f4e071c8facfb719630454d65d1e6765e
Saman <s.sattari@mydigipay.com>
2021-05-07 13:34:34 +0430

    created local module to save and retrieve data from local db

---
+++ app/src/main/java/com/turkurt656/filmasyon/KoinModules.kt
@@ -0,0 +1,16 @@
+package com.turkurt656.filmasyon
+
+import com.turkurt656.data.local.di.dataStoreModule
+import com.turkurt656.library.core.di.GlobalQualifiers.IS_DEBUG
+import org.koin.dsl.module
+
+val appModule = module {
+
+    factory(IS_DEBUG) { BuildConfig.DEBUG }
+
+}
+
+val koinModules = listOf(
+    appModule,
+    dataStoreModule,
+)
---
+++ data/local/src/main/java/com/turkurt656/data/local/di/DataStoreModule.kt
@@ -0,0 +1,9 @@
+package com.turkurt656.data.local.di
+
+import org.koin.dsl.module
+
+private const val APP_DATA_STORE = "app_data_store"
+
+val dataStoreModule = module {
+
+}
